f2572c424fd1a4a8cc33686ae069b4999c4c2694
Joern Putschke <joern.putschke@wayne.edu>
2022-10-27 16:02:25 -0400

    Custom ROOT event writer ...
    as an example of ROOT integration, adapted from the old jetscape DEV branch.

---
+++ src/root/JetScapeRootLinkDef.h
@@ -0,0 +1,16 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+//#pragma link C++ nestedclass;
+//#pragma link C++ nestedtypedef;
+
+#pragma link C++ class JetScapeRootParton+;
+#pragma link C++ class JetScapeRootPartonShower+;
+#pragma link C++ class JetScapeRootVertex+;
+#pragma link C++ class JetScapeRootEvent+;
+#pragma link C++ class JetScapeWriterRoot+;
+
+
+#endif
