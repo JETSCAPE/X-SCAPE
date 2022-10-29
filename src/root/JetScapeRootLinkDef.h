8bf6c723f1e4b97d41b067d61bf87fa44610cf06
Joern Putschke <joern.putschke@wayne.edu>
2022-10-28 21:04:29 -0400

    Minor updates ...

--- src/root/JetScapeRootLinkDef.h
+++ src/root/JetScapeRootLinkDef.h
@@ -19,21 +19,3 @@
 #pragma link C++ class Jetscape::Hadron+;
 
 #endif
-/*
-#ifdef __CINT__
-
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-//#pragma link off nestedclass;
-//#pragma link off nestedtypedef;
-
-#pragma link C++ class JetScapeRootParton+;
-#pragma link C++ class JetScapeRootPartonShower+;
-#pragma link C++ class JetScapeRootVertex+;
-#pragma link C++ class JetScapeRootEvent+;
-#pragma link C++ class JetScapeWriterRoot+;
-
-
-#endif
-*/
