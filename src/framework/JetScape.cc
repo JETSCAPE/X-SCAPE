7b47b5b9566765cb3e5d960789bef5b39c4eb359
Joe Latessa <jlatessa@gmail.com>
2022-11-01 14:27:25 -0400

    updated xml warning to terminate and require xml main default tag

--- src/framework/JetScape.cc
+++ src/framework/JetScape.cc
@@ -121,8 +121,10 @@ void JetScape::recurseToSearch(std::vector<std::string> &elems, tinyxml2::XMLEle
   if (nextElement != nullptr)
     recurseToSearch(elems, nextElement);
 
-  if (!std::binary_search(elems.begin(), elems.end(), uElement->Name()))
-    JSWARN << "user XML element <" << uElement->Name() << "> not found in main XML file.";
+  if (!std::binary_search(elems.begin(), elems.end(), uElement->Name())) {
+    JSWARN << "User XML tag <" << uElement->Name() << "> is unrecognized. A default entry in the main XML file is required.";
+    exit(-1);
+  }
 }
 
 //________________________________________________________________
