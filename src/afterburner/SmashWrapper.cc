50197f8bec5f9756a8b49cfa3dc7df7709aec70b
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2024-03-21 10:11:01 -0400

    Change hadron status to 27 in GetCurrentHadronList function

--- src/afterburner/SmashWrapper.cc
+++ src/afterburner/SmashWrapper.cc
@@ -184,7 +184,7 @@ std::vector<Hadron> SmashWrapper::GetCurrentHadronList() const {
 
   for (const auto &particle : *smash_particles) {
     const int hadron_label = 0;
-    const int hadron_status = -1;
+    const int hadron_status = 27;
     const int hadron_id = particle.pdgcode().get_decimal();
     smash::FourVector p = particle.momentum(), r = particle.position();
     const FourVector hadron_p(p.x1(), p.x2(), p.x3(), p.x0()),
