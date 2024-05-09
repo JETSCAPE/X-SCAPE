17c168c1518b2c160d06d87440a8471111a7545d
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2024-05-09 16:45:02 -0400

    Change current MUSIC version to test branch

--- external_packages/get_music.sh
+++ external_packages/get_music.sh
@@ -15,13 +15,14 @@
 
 folderName="music"
 # commitHash="52b0454b0a07a76f26c8138245c0b3c54d00af65" # for xscape 1.1
-commitHash="db320898c4a9bd99ed21aa9dbb7c78e2cb0729bd" # for xscape 1.1.1
+#commitHash="db320898c4a9bd99ed21aa9dbb7c78e2cb0729bd" # for xscape 1.1.1
 
 # download the code package
 rm -fr $folderName
-git clone https://github.com/MUSIC-fluid/MUSIC.git --branch JETSCAPE $folderName
-cd $folderName
-git checkout $commitHash
+#git clone https://github.com/MUSIC-fluid/MUSIC.git --branch JETSCAPE $folderName
+git clone https://github.com/MUSIC-fluid/MUSIC.git --branch hendrik_XSCAPE_dynamical_sources $folderName
+#cd $folderName
+#git checkout $commitHash
 
 ### ALTERNATIVE VERSION
 ### Download a zipped snapshot
