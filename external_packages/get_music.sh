e70c1ca1f732f33dc75c412a36044fd21318e6b9
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2025-07-15 12:37:25 -0400

    Fix filename in EOS download

--- external_packages/get_music.sh
+++ external_packages/get_music.sh
@@ -21,7 +21,7 @@ git clone https://github.com/MUSIC-fluid/MUSIC.git -b XSCAPE $folderName
 cd $folderName
 git checkout $commitHash
 cd EOS
-bash download_hotQCD.sh binary_SMASH
+bash download_hotQCD.sh SMASH_binary
 
 ### ALTERNATIVE VERSION
 ### Download a zipped snapshot
