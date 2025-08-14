b1a6a02203b83810b76689c7e677b0bace0be5ec
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2025-08-14 09:17:44 -0400

    Fix MUSIC status bug for FO

--- external_packages/get_music.sh
+++ external_packages/get_music.sh
@@ -15,7 +15,7 @@
 
 # using a commit from the MUSIC repository that is compatible with the current X-SCAPE version
 folderName="music"
-commitHash="c95ef8d27b8f8981a8491337b90aebaf517f0c98"
+commitHash="562de76ef21ee287465c2e49343aff78fac00e9c"
 
 git clone https://github.com/MUSIC-fluid/MUSIC.git -b XSCAPE $folderName
 cd $folderName
