ffe361ecd6c637d363e8a1385768a824b72582f5
Chun <chunshen@wayne.edu>
2025-09-07 10:59:50 -0400

    update 3D-Glauber version
    avoid uninitialized values

--- external_packages/get_3dglauber.sh
+++ external_packages/get_3dglauber.sh
@@ -14,7 +14,7 @@
 ##############################################################################
 
 folderName="3dMCGlauber"
-commitHash="cdbb7e05457f99ae4c275f9619bf01fb3499c7cf"
+commitHash="71116fefcd6a7fd8db87f31eb95fe8f4cea6d152"
 # download the code package
 rm -fr $folderName
 git clone https://github.com/chunshen1987/3dMCGlauber.git --branch JETSCAPE $folderName
