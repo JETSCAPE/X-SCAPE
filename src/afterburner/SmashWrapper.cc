b898595cdc5b8fbcd34699f466c1b0aaa1f5f404
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2025-09-30 13:47:46 -0400

    Fix Delta_Time in Afterburner

--- src/afterburner/SmashWrapper.cc
+++ src/afterburner/SmashWrapper.cc
@@ -79,7 +79,7 @@ void SmashWrapper::InitTask() {
                                                      tabulations_path);
   }
 
-  const double delta_t_sm = GetXMLElementDouble({"IS", "SMASH", "Delta_Time"});
+  const double delta_t_sm = GetXMLElementDouble({"Afterburner", "SMASH", "Delta_Time"});
   config.set_value(smash::InputKeys::gen_deltaTime, delta_t_sm);
 
   // Enforce timestep compatibility (temporarily)
