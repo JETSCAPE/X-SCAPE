157782107edae510f3f1416fc289cecc3b079c5d
Joe Latessa <latessa@users.noreply.github.com>
2025-11-27 11:43:10 -0500

    modify macos workflow and get_smash troubleshoot eigen not found

--- external_packages/get_smash.sh
+++ external_packages/get_smash.sh
@@ -19,11 +19,31 @@ git clone --depth=1 https://github.com/smash-transport/smash.git --branch SMASH-
 # 2) Compile SMASH
 (
 cd smash/smash_code
-mkdir build
+mkdir -p build
 cd build
-cmake .. -DPythia_CONFIG_EXECUTABLE=${PYTHIA8DIR}/bin/pythia8-config -DCMAKE_PREFIX_PATH="${HEPMC3_DIR};${EIGEN3_ROOT}"
+
+# Ensure Eigen path is set
+if [ -z "${EIGEN3_ROOT}" ]; then
+	if command -v brew >/dev/null 2>&1; then
+		export EIGEN3_ROOT="$(brew --prefix eigen)/include/eigen3"
+	fi
+fi
+
+# Locate HepMC3 and Eigen. Add both if present.
+prefix_path="${HEPMC3_DIR};${EIGEN3_ROOT}"
+
+cmake .. \
+	-DPythia_CONFIG_EXECUTABLE=${PYTHIA8DIR}/bin/pythia8-config \
+	-DCMAKE_PREFIX_PATH="${prefix_path}" \
+	-DEIGEN3_ROOT="${EIGEN3_ROOT}" || exit 1
+
 num_cores=${1:-1}
 echo "Compiling SMASH using ${num_cores} cores."
-make -j${num_cores} smash_shared
+
+if make -n | grep -q "smash_shared"; then
+	make -j${num_cores} smash_shared || exit 1
+else
+	make -j${num_cores} || exit 1
+fi
 )
 export SMASH_DIR="$(pwd)/smash/smash_code"
