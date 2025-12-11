8c494e2cfd491728cf2b77e6040dd4a2649a35c0
Chun <chunshen@wayne.edu>
2025-12-11 07:50:58 -0500

    update the 3D-Glauber for the remnant treatment

--- external_packages/get_3dglauber.sh
+++ external_packages/get_3dglauber.sh
@@ -14,7 +14,7 @@
 ##############################################################################
 
 folderName="3dMCGlauber"
-commitHash="22a70de96a86f1bb7c0aefef137fb2fe3ef81aa9"
+commitHash="a88ca1441b729f0e9845da624057ae3a095dcb9e"
 # download the code package
 rm -fr $folderName
 git clone https://github.com/chunshen1987/3dMCGlauber.git --branch JETSCAPE $folderName
