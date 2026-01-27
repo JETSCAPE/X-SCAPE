e31870163c4ee1efca0791fe04700827b0ed07fc
Ritoban Datta <hl9735@wayne.edu>
2026-01-27 09:42:25 -0500

    Change ModificationCorr from 999 to 1.0

--- src/jet/Matter.cc
+++ src/jet/Matter.cc
@@ -149,7 +149,7 @@ void Matter::InitTask() {
   Lambda_QCD = GetXMLElementDouble({"Eloss","lambdaQCD"});
 
   ModificationFactor = GetXMLElementDouble({"Eloss", "ModificationFactor"});
-  ModificationCorr = 999;
+  ModificationCorr = 1.0;
 
   if(vir_factor < 0.0) {
     JSWARN << "vir_factor should not be negative";
