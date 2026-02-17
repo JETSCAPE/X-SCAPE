193b3d6e48c1347958cac3e995a5e5399d23feaf
Joe Latessa <latessa@users.noreply.github.com>
2026-02-17 14:35:36 -0500

    implement OMP threads for sample 2p1d and 3p1d

--- src/hadronization/HybridHadronization.cc
+++ src/hadronization/HybridHadronization.cc
@@ -539,7 +539,7 @@ void HybridHadronization::DoHadronization(vector<vector<shared_ptr<Parton>>>& sh
 	  brick.brick_length_width(brickL,brickL);
 	  brick.brick_flow(0., 0., 0.);
     brick.brick_Tc(hydro_Tc);
-	  brick.samplebrick();
+	  brick.sampl_brick();
 
 	  JSINFO << "A " << brickL << " fm brick was sampled, generating " << brick.nTot() << " partons (" << brick.th_nL() << " light, " << brick.th_nS() << " strange).";
 
