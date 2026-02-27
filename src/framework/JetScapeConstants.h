c57adfd6c2639b30621e1f8c2b67fd7050ad0e1b
Joe Latessa <latessa@users.noreply.github.com>
2026-02-27 10:54:17 -0500

    add doxygen documentation for JetScapeBanner and JetScapeConstants

--- src/framework/JetScapeConstants.h
+++ src/framework/JetScapeConstants.h
@@ -19,21 +19,28 @@
 
 namespace Jetscape {
 
-// define the usual constants
+/// Constant: Pi value
 static double pi = 3.141592653589793;
 
+/// Constant: Number of flavors
 static double nf = 3.0;
 
+/// Constant: Color factor associated with gluon emission from a quark
 static double Cf = 4.0 / 3.0;
 
+/// Constant: XXX
 static double Tf = 0.5;
 
+/// Constant: Color factor associated with gluon emission from a gluon
 static double Ca = 3.0;
 
+/// Constant: Number of colors
 static double Nc = 3.0;
 
+/// Constant: hbarC in GeV fm
 static const double hbarC = 0.197327053;
 
+/// Constant: Conversion factor from fm to GeV^-1
 static const double fmToGeVinv = 1.0 / hbarC;
 
 static double zeta3 = 1.20206;
@@ -42,11 +49,10 @@ static double mu = 0.722;
 
 /* When the code becomes really accurate, a more accurate value for this can be
  * used  */
-
-/*  the following is the maximum value from the standard C++ random number
- * generator */
+ /// Constant:: Maximum value from the standard C++ random number generator
 static double maxN = double(pow(2.0, 31.0) - 1.0);
 
+/// Constant: Maximum double value
 static double a_very_large_number = maxN;
 
 /* the following 2 lines control the error in the analytical part of the
@@ -66,29 +72,38 @@ static double s_approx = 0.01;
 
 static double E_minimum = 1.0;
 
-static double rounding_error = 1e-6;  // slightly more than float precision
+/// Constant: Rounding error, slightly more than float precision
+static double rounding_error = 1e-6;
 
 /**************************************************************************************/
-
-/* the standard PDG particle id codes for the gluon and the d quark */
+/// Constant: Gluon PDG code
 static int gid = 21;
 
+/// Constant: Down quark PDG code
 static int qid = 1;
 
+/// Constant: Up quark PDG code
 static int uid = 2;
 
+/// Constant: Strange quark PDG code
 static int did = 1;
 
+/// Constant: Charm quark PDG code
 static int sid = 3;
 
+/// Constant: Bottom quark PDG code
 static int cid = 4;
 
+/// Constant: Top quark PDG cod
 static int bid = 5;
 
+/// Constant: Photon PDG code
 static int photonid = 22;
 
+/// Constant: Z boson PDG code
 static int Zid = 23;
 
+/// Constant: W+ boson PDG code, use -24 for W-
 static int Wid = 24;  // refers to W+, use -24 for W-
 /*******************************************************************/
 
